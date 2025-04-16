2f1422d49eb4c4d37658e691ab62b945f0acd80a
Kauan Costa <kauan7622@gmail.com>
2025-04-15 23:30:24 -0300

    feat: add error handling utility for Axios responses

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,12 @@
+import axios from 'axios';
+
 export const cn = (...inputs: (string | undefined)[]) => {
   return inputs.filter(Boolean).join(' ');
+};
+
+export const handleMessageError = (error: unknown) => {
+  if (axios.isAxiosError(error)) {
+    return error?.response?.data?.error?.message ?? 'Erro desconhecido';
+  }
+  return 'Erro desconhecido';
 };
